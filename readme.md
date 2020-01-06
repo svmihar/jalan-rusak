fd5a09352cfa1781e70a993b7fcb742e4b247bd7
svmihar <svmihar@gmail.com>
2020-01-06 16:37:19 +0700

    :tada: new video to image per frame

---
+++ readme.md
@@ -0,0 +1,5 @@
+
+## dataset
+- buat `./data/`
+- https://drive.google.com/drive/folders/1OR6LU-lcdHZpU42hJ2OhFsYdYltkt7zW?usp=sharing
+
---
+++ video2frame.py
@@ -0,0 +1,20 @@
+import cv2
+from tqdm import tqdm
+from fire import Fire
+
+
+def main(video_file, path_to_save):
+    print(cv2.__version__)
+    vidcap = cv2.VideoCapture(video_file)
+    property_id = int(cv2.CAP_PROP_FRAME_COUNT)
+    length = int(cv2.VideoCapture.get(vidcap, property_id))
+    success, image = vidcap.read()
+    count = 0
+    success = True
+    for i in tqdm(range(length)):
+        cv2.imwrite(f"{path_to_save}/frame_{count}.jpg}", image)
+        success, image = vidcap.read()
+
+
+if __name__ == "__main__":
+    Fire(main)
